e7445cacb06e7bcbb4719a8fb7664087e001db73
dannyrgd <danny.ramirezgd@gmail.com>
2022-04-22 17:30:39 -0400

    added models for admin unit and request, created type defintions for all three, made a couple of resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
+const { Admin, Unit } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
+        },
+        units: async () => {
+            return Unit.find()
+                .select('-__v -password')
+                .populate('requests')
+        },
+        admins: async () => {
+            return Admin.find()
+                .select('-__v -password')
+                .populate('requests')
+                .populate('units')
         }
     },
     Mutation: {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,19 +1,44 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-   
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Unit {
+    _id: ID
+    unitNumber: Int
+    email: String
+    password: String
+    requestCount: Int
+    requests: [Request]
+  }
+  
+  type Request {
+    _id: ID
+    requestBody: String
+    unit: String
+    createdAt: String
+  }
 
-    type Query {
-        me: User
-      }
-    
-      type Mutation {
-        login(email: String!, password: String!): Auth
-      }
+  type Admin {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    isAdmin: Boolean
+    requestCount: Int
+    unitCount: Int
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
