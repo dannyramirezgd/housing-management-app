e648a4a06730e2a4bdbb6bdd4f79659eace5dae9
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-28 00:39:16 -0400

    need to figure out how I can diff between Admin and User where admin login directs to adminHome and user login directs to unitInfo

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -35,10 +35,12 @@ class AuthService {
     // set token to localStorage and reload page to homepage
     login(idToken) {
         //saves user token to localStorage
+
         localStorage.setItem('id_token', idToken);
 
-        window.location.assign('/');
+        window.location.assign('/adminHome');
     }
+
 
     //clear token from localStorage and force logout with reload
     logout() {
