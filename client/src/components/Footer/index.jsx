4a725e614dfaa6b999e19cf36fe2e55225d3570a
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-29 03:27:20 -0400

    footer done

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook as facebook,
+  faInstagram as instagram,
+  faTwitter as twitter,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faIcons as amenities,
+  faEnvelope as contact,
+  faAddressCard as about,
+  faFileImport as documents,
+} from '@fortawesome/free-solid-svg-icons';
+
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by</div>
+    <footer
+      className={styles.footerContainer}
+      style={{ backgroundColor: '#4682B4' }}
+    >
+      <div>
+        <FontAwesomeIcon icon={facebook} className={styles.icon} />
+        <FontAwesomeIcon icon={instagram} className={styles.icon} />
+        <FontAwesomeIcon icon={twitter} className={styles.icon} />
+      </div>
+      <div>
+        <Link to="/amenities" >
+          <FontAwesomeIcon icon={amenities} className={styles.infoIcon} />
+          AMENITIES
+        </Link>
+        <Link to="/about" className={styles.info}>
+          <FontAwesomeIcon icon={about} className={styles.infoIcon} />
+          ABOUT
+        </Link>
+        <Link to="/documents" className={styles.info}>
+          <FontAwesomeIcon icon={documents} className={styles.infoIcon} />
+          DOCUMENTS
+        </Link>
+        <Link to="/contact" className={styles.info}>
+          <FontAwesomeIcon icon={contact} className={styles.infoIcon} />
+          CONTACT US
+        </Link>
+      </div>
     </footer>
   );
 };
