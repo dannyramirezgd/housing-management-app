6847c4b1fe74a656f5d05bf5fd6d25ef28370468
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-29 20:49:27 -0400

    modal btn moved into jumbotron

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,9 @@
-import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import SignUpForm from '../components/Signup';
 import Hero from '../components/Hero';
 
 const Home = () => {
-  const [showSignModal, setShowSignModal] = useState(false);
   return (
     <main>
       <Hero />
-      <button onClick={() => setShowSignModal(true)}>Create an account</button>
-      <Modal
-        size="lg"
-        show={showSignModal}
-        onHide={() => setShowSignModal(false)}
-      >
-        <SignUpForm handleModalClose={() => setShowSignModal(false)} />
-      </Modal>
     </main>
   );
 };
