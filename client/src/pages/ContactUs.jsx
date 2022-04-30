b0b64019caee2e53d0634cc88055d2123c087150
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-29 23:57:30 -0400

    contact us page got erromsg handling

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,123 +1,232 @@
-import React, { useState } from "react";
-import { validateEmail, validateInput } from "../../utils/helpers";
-
+import React, { useState } from 'react';
+import { validateEmail, validateInput } from '../utils/helpers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser as user,
+  faEnvelope as mail,
+} from '@fortawesome/free-regular-svg-icons';
+import bgImg from '../image/NewHouseLifeStockPhoto_6.jpg';
 
 const ContactUs = () => {
-  const [formState, setFormState] = useState({ name: "", email: "", message: "" });
-  const [errorEmail, setErrorEmail] = useState("");
-  const [errorName, setErrorName] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorName, setErrorName] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
   const { name, email, message } = formState;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!errorEmail && !errorName && !errorMsg) {
-      console.log("Submit Form", formState);
+      console.log('Submit Form', formState);
     }
   };
 
   const handleNameChange = (e) => {
-    if (e.target.name === "name") {
+    if (e.target.name === 'name') {
       const isValid = validateInput(e.target.value);
       if (!isValid) {
         setErrorName(`${e.target.name} is required`);
       } else {
-        setErrorName("");
+        setErrorName('');
       }
     }
     if (!errorName) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log("Handle Form", formState);
+      console.log('Handle Form', formState);
     }
   };
 
   const handleEmailChange = (e) => {
-    if (e.target.name === "email") {
+    if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
-        setErrorEmail("Email is invalid. Try Again!");
+        setErrorEmail('Email is invalid. Try Again!');
       } else {
-        setErrorEmail("");
+        setErrorEmail('');
       }
     }
     if (!errorEmail) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log("Handle Form", formState);
+      console.log('Handle Form', formState);
     }
   };
 
   const handleMsgChange = (e) => {
-    if (e.target.name === "message") {
+    if (e.target.name === 'message') {
       const isValid = validateInput(e.target.value);
       if (!isValid) {
         setErrorMsg(`${e.target.name} is required`);
       } else {
-        setErrorMsg("");
+        setErrorMsg('');
       }
     }
     if (!errorName) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log("Handle Form", formState);
+      console.log('Handle Form', formState);
     }
   };
 
   const backHome = () => {
-    window.location.reload("/");
+    window.location.reload('/');
   };
 
   return (
-    <section>
-      <h1 className="is-size-3-mobile is-size-3 has-text-centered mt-3">Contact Me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div className="columns is-mobile">
-          <div className="column is-three-fifths is-offset-one-fifth field">
-            <label className="label" htmlFor="name">
-              Name
-            </label>
-            <div className="control has-icons-left">
-              <input className={errorName ? "input is-danger" : "input"} type="text" name="name" defaultValue={name} placeholder="name" onBlur={handleNameChange} />
-              <span className="icon is-small is-left">
-                <i className="fa-solid fa-user"></i>
-              </span>
-            </div>
-            {errorName && <p className="help is-danger">Name is required</p>}
-
-            <label className="label mt-1" htmlFor="email">
-              Email
-            </label>
-            <div className="control has-icons-left">
-              <input className={errorEmail ? "input is-danger" : "input"} type="email" name="email" defaultValue={email} placeholder="email" onBlur={handleEmailChange} />
-              <span className="icon is-small is-left">
-                <i className="fas fa-envelope"></i>
-              </span>
-            </div>
-            {errorEmail && <p className="help is-danger">This email is invalid</p>}
-
-            <label className="label mt-1" htmlFor="message">
-              Message
-            </label>
-            <div className="control">
-              <textarea className={errorMsg ? "textarea is-danger" : "textarea"} name="message" defaultValue={message} placeholder="Textarea" onBlur={handleMsgChange}></textarea>
-            </div>
-            {errorMsg && <p className="help is-danger">Message is required</p>}
-
-            <div className="field is-grouped pt-3">
-              <div className="control">
-                <button className="button is-link" type="submit">
-                  Submit
-                </button>
+    <div
+      style={{
+        backgroundImage: `url(${bgImg})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100vh',
+        fontFamily: 'Gruppo',
+        color: '#f5f5f5',
+        fontWeight: 'bold',
+        fontSize: '1.5rem',
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: 'rgba(0,0,0,0.5)',
+          width: '100%',
+          height: '100vh',
+        }}
+      >
+        <section className="container d-flex flex-column align-items-center justify-content-center">
+          <h1
+            className="text-center mt-5"
+            style={{ fontFamily: 'Abel', fontWeight: 'bold', fontSize: '3rem' }}
+          >
+            Contact Us
+          </h1>
+          <form id="contact-form" className="col-6" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label className="label" htmlFor="name">
+                Name
+              </label>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="basic-addon1">
+                    <FontAwesomeIcon icon={user} size="2x" />
+                  </span>
+                </div>
+                <input
+                  className={
+                    errorName
+                      ? 'form-control border border-danger'
+                      : 'form-control'
+                  }
+                  type="text"
+                  name="name"
+                  defaultValue={name}
+                  placeholder="name"
+                  onBlur={handleNameChange}
+                />
               </div>
-              <div className="control">
-                <button className="button is-link is-light" onClick={backHome}>
-                  Cancel
-                </button>
+              {errorName && (
+                <p
+                  style={{
+                    color: 'red',
+                    fontFamily: 'Patrick Hand',
+                    fontSize: '0.7em',
+                  }}
+                >
+                  Name is required
+                </p>
+              )}
+              <div className="form-group">
+                <label className="label" htmlFor="email">
+                  Email
+                </label>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text" id="basic-addon2">
+                      <FontAwesomeIcon icon={mail} size="2x" />
+                    </span>
+                  </div>
+                  <input
+                    className={
+                      errorEmail
+                        ? 'form-control border border-danger'
+                        : 'form-control'
+                    }
+                    type="email"
+                    name="email"
+                    defaultValue={email}
+                    placeholder="email"
+                    onBlur={handleEmailChange}
+                  />
+                </div>
+                {errorEmail && (
+                  <p
+                    style={{
+                      color: 'red',
+                      fontFamily: 'Patrick Hand',
+                      fontSize: '0.7em',
+                    }}
+                  >
+                    This email is invalid
+                  </p>
+                )}
+              </div>
+
+              <div className="form-group">
+                <label className="label" htmlFor="email">
+                  Message
+                </label>
+                <div className="input-group mb-3">
+                  <textarea
+                    style={{ resize: 'none', height: '300px' }}
+                    className={
+                      errorEmail
+                        ? 'form-control border border-danger'
+                        : 'form-control'
+                    }
+                    type="text"
+                    name="message"
+                    defaultValue={message}
+                    placeholder="How can we help you?"
+                    onBlur={handleMsgChange}
+                  />
+                </div>
+                {errorMsg && (
+                  <p
+                    style={{
+                      color: 'red',
+                      fontFamily: 'Patrick Hand',
+                      fontSize: '0.7em',
+                    }}
+                  >
+                    Message is required
+                  </p>
+                )}
+              </div>
+
+              <div className="field is-grouped pt-3">
+                <div className="control">
+                  <button className="button is-link" type="submit">
+                    Submit
+                  </button>
+                </div>
+                <div className="control">
+                  <button
+                    className="button is-link is-light"
+                    onClick={backHome}
+                  >
+                    Cancel
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </form>
-    </section>
+          </form>
+        </section>
+      </div>
+    </div>
   );
-}
+};
 
 export default ContactUs;
