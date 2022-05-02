454fd61def2b8f2e2c7436b36e376eefe19992ba
Daniel Kim <daniel.d.kim13@gmail.com>
2022-05-02 18:53:40 -0400

    i did something.. carousel

--- client/src/components/About/Constants.jsx
+++ client/src/components/About/Constants.jsx
@@ -5,18 +5,18 @@ export const infos = [
     comment: 'We are always here 24/7 like 7-eleven !!',
   },
   {
-    image: '',
+    image: 'slideTwo.jpg',
     title: "Daniel 'DK' Kim",
     comment:
       "For six months, I studied and learned so much... yet I'm staring at my monitor DUMBFOUNDED!",
   },
   {
-    image: '',
+    image: 'slideThree.jpg',
     title: 'Danny Ramirez',
     comment: "Danny's comments here!!",
   },
   {
-    image: '',
+    image: 'slideFour.jpg',
     title: 'Chris Backes',
     comment: "Chris's comments here!!",
   },
--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -1,11 +1,58 @@
-import SingleAbout from './SingleAbout';
-
+import { infos } from './Constants';
+import { Carousel } from 'react-bootstrap';
 
 const About = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require(`../../image/about/${infos[0].image}`)}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>{infos[0].title}</h3>
+          <p>{infos[0].comment}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require(`../../image/about/${infos[1].image}`)}
+          alt="Second slide"
+        />
 
-    return (
-        <SingleAbout />
-    )
-}
+        <Carousel.Caption>
+          <h3>{infos[1].title}</h3>
+          <p>{infos[1].comment}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require(`../../image/about/${infos[2].image}`)}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>{infos[2].title}</h3>
+          <p>{infos[2].comment}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require(`../../image/about/${infos[3].image}`)}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>{infos[3].title}</h3>
+          <p>{infos[3].comment}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
 
 export default About;
