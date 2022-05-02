0d542e70cadc27e8dc2e2aee48e0fdaae08a3e56
Daniel Kim <daniel.d.kim13@gmail.com>
2022-05-02 18:12:38 -0400

    about pictures and constants made

---
+++ client/src/components/About/Constants.jsx
@@ -0,0 +1,22 @@
+export const infos = [
+    { 
+        image: 'slideOne.jpg',
+        title: "New House Life",
+        comment: "We are always here 24/7 like 7-eleven !!"
+    },
+    { 
+        image: '',
+        title: "Daniel 'DK' Kim",
+        comment: "For six months, I studied and learned so much... yet I'm staring at my monitor DUMBFOUNDED!"
+    },
+    { 
+        image: '',
+        title: "Danny Ramirez",
+        comment: "Danny's comments here!!"
+    },
+    { 
+        image: '',
+        title: "Chris Backes",
+        comment: "Chris's comments here!!"
+    }, 
+]
