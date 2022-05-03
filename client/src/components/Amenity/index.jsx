8e4928545332d881481e58af84c4c1a2f9a8adff
Daniel Kim <daniel.d.kim13@gmail.com>
2022-05-03 01:48:06 -0400

    not the best but also not the worst.. lol

--- client/src/components/Amenity/index.jsx
+++ client/src/components/Amenity/index.jsx
@@ -1,10 +1,39 @@
 import Neighborhood from './Neighborhood';
+import pool from '../../image/amenities/swimming.jpg';
+import bbq from '../../image/amenities/bbq.jpg';
+import office from '../../image/amenities/office.jpg';
+import { Carousel } from 'react-bootstrap';
 
 const Amenity = () => {
   return (
-    <div>
+    <>
+      <Carousel className="my-5">
+        <Carousel.Item>
+          <img className="d-block w-100" src={pool} alt="Swimming Pool" />
+          <Carousel.Caption>
+            <h3>Indoor Swimming Pool</h3>
+            <p>The pool water smells decent enough for you to enjoy!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bbq} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>Serve it up!</h3>
+            <p>You can bbq all day every day as a resident!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={office} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Flexible shared workspaces</h3>
+            <p>We have both Mac and PC. Choose your flavor~</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
       <Neighborhood />;
-    </div>
+    </>
   );
 };
 
